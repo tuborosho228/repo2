0541c221054b9d97bd7a06aa9a53852af3e1ec12
user <uczen@ANTONI>
2025-10-03 15:34:53 +0200

    wiadomosc

---
+++ StacjaPogodowa/DataBase/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    internal class Weather
+    {
+        public int Id { get; set; }
+
+        public string MeasuredBy { get; set; } = string.Empty;
+
+        public DateTime MeasurementDate { get; set; }
+    }
+}
